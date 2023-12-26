84b9a2ad961d6f211660cb0f51e980d1a1bff545
JamesWalsh78 <154488313+JamesWalsh78@users.noreply.github.com>
2023-12-26 11:16:18 +0000

    Automated commit and push

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
-// Get the current date
-const currentDate = new Date();
-
-// Get the current month and year
-const currentMonth = currentDate.getMonth();
-const currentYear = currentDate.getFullYear();
-
-// Get the container elements
-const calendarContainer = document.getElementById("calendar-container");
-const monthHeading = document.getElementById("month-heading");
-const calendarDays = document.getElementById("calendar-days");
-
-// Array of month names
-const monthNames = [
-  "January", "February", "March", "April",
-  "May", "June", "July", "August",
-  "September", "October", "November", "December"
-];
-
-// Set the month heading
-monthHeading.textContent = `${monthNames[currentMonth]} ${currentYear}`;
-
-// Get the first day of the month and the total days in the month
-const firstDay = new Date(currentYear, currentMonth, 1).getDay();
-const totalDays = new Date(currentYear, currentMonth + 1, 0).getDate();
-
-// Generate the calendar days
-for (let i = 0; i < firstDay; i++) {
-  const emptyDay = document.createElement("div");
-  emptyDay.classList.add("day", "empty");
-  calendarDays.appendChild(emptyDay);
-}
-
-for (let day = 1; day <= totalDays; day++) {
-  const calendarDay = document.createElement("div");
-  calendarDay.classList.add("day");
-  calendarDay.textContent = day;
-  calendarDays.appendChild(calendarDay);
-}
-
-// ... (any additional logic or customization)
+// Function to update the calendar
+const updateCalendar = () => {
+    // Set the month heading
+    monthHeading.textContent = `${monthNames[currentDate.getMonth()]} ${currentDate.getFullYear()}`;
+  
+    // Clear previous calendar days and headings
+    calendarDays.innerHTML = "";
+  
+    // Get the first day of the month and the total days in the month
+    const firstDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay();
+    const totalDays = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+  
+    // Array of day names
+    const dayNames = Array.from({ length: 7 }, (_, i) => {
+      const tempDate = new Date(0, 0, 1 + i);
+      return tempDate.toLocaleDateString('en-US', { weekday: 'short' });
+    });
+  
+    // Generate day name headings
+    for (const dayName of dayNames) {
+      const dayHeading = document.createElement("div");
+      dayHeading.classList.add("day-heading");
+      dayHeading.textContent = dayName;
+      calendarDays.appendChild(dayHeading);
+    }
+  
+    // Generate the calendar days
+    for (let i = 0; i < firstDay; i++) {
+      const emptyDay = document.createElement("div");
+      emptyDay.classList.add("day", "empty");
+      calendarDays.appendChild(emptyDay);
+    }
+  
+    for (let day = 1; day <= totalDays; day++) {
+      const calendarDay = document.createElement("div");
+      calendarDay.classList.add("day");
+      calendarDay.textContent = day;
+      calendarDays.appendChild(calendarDay);
+    }
+  
+    // ... (any additional logic or customization)
+  };
+  
